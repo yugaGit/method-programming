af316fdab3fe7fc0cc4ade43d3f1a9d2b484f256
Yugaraj.B <114376104+yugaGit@users.noreply.github.com>
2022-09-29 22:08:13 +0530

    Add files via upload
    In this  program i have created two methods in vikram  and calling the methods in new class Kamal

---
+++ Kamal.java
@@ -0,0 +1,11 @@
+  public class Kamal{
+ 
+public static void main(String[]args){
+    vikram song=new vikram();
+     song.Aandavaa();
+     song.Aandavaa2();
+
+}
+}
+
+ 
---
+++ vikram.java
@@ -0,0 +1,13 @@
+public class vikram{
+
+ public void Aandavaa(){
+   String num1="once upon a time there lived a goast ";
+   String num2="he was known to be a killer and feared the most ";
+   System.out.print(num1+".."+num2);
+}
+  public void Aandavaa2(){
+   String num3="The eagle is coming,you better start running";
+   String num4="his blood is rushing stunning and gunning";
+System.out.println("\n"+num3+".."+num4);
+}
+}
